562ef4aeed0e143ae767c7d6b50a9514c40d2964
George-kiki <504110744@qq.com>
2021-04-14 17:11:06 +0800

    project init

---
+++ src/views/singer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    222
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'singer'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/top-list.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    444
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'top-list'
+}
+</script>
+
+<style scoped>
+
+</style>
